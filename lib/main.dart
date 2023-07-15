d54b1fab6ffbac496795f01bad67cba17a40074f
sinhle3008 <sinhle.dev@gmail.com>
2023-07-15 14:41:33 +0700

    hoan thanh

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:lab_06_distini/storybrain.dart';
+
+StoryBrain storyBrain = StoryBrain();
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StoryPage(),
+      theme: ThemeData.light(),
+    );
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        //thêm back ground cho app
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("images/background.png"),
+          fit: BoxFit.cover,
+        )),
+        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 15),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        storyBrain.nextStory(1);
+                      });
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      textStyle: TextStyle(fontSize: 20),
+                    ),
+                    child: Text(storyBrain.getChoice1()),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      textStyle: TextStyle(fontSize: 20),
+                    ),
+                    child: Text(storyBrain.getChoice2()),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
